3da6078df1d6e5f82ee6224d46e98c31d1b218fd
Keigo MIZUTANI <keigo@jlab.org>
2019-12-17 14:27:45 -0500

    Add copy.sh

---
+++ copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp Plot_Browser.html /group/halld/www/halldweb/html/data_monitoring/t.html
+cp js_utilities/rcdb_browser.php /group/halld/www/halldweb/html/data_monitoring/js_utilities/t.php
