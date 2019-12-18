ddfe428fe725ebb77ef1c322fa27e30012f6290e
Keigo MIZUTANI <keigo@jlab.org>
2019-12-18 02:36:59 -0500

    Fix a bug in mk_json.py
    Replace rcdb_browser.php!

--- js_utilities/rcdb_browser.php
+++ js_utilities/rcdb_browser.php
@@ -1,70 +1,13 @@
 <?php
 
-$data = array();
+$data_json = file_get_contents('../test2.json');
 
-$RunPeriods = scandir("/work/halld2/data_monitoring/");
-// file_put_contents("/u/group/halld/www/halldweb/html/data_monitoring/debug.txt", "test\n");
-// file_put_contents("/u/group/halld/www/halldweb/html/data_monitoring/debug.txt", print_r($RunPeriods, true), FILE_APPEND);
+// $data = json_decode($data_json, true);
 
-array_shift($RunPeriods);
-array_shift($RunPeriods);
-array_pop($RunPeriods);
-foreach ($RunPeriods as &$runp) {
-  $runpData = array();
-  // echo $runp . "<br>";
-  // echo "-----------<br>";
-  $versions = scandir("/work/halld2/data_monitoring/" . $runp . "/");
-  array_shift($versions);
-  array_shift($versions);
+// echo json_encode($data, JSON_PRETTY_PRINT);
+echo $data_json;
 
-  foreach ($versions as &$ver) {
-    // echo $ver . "<br>";
-    $runs = scandir("/work/halld2/data_monitoring/" . $runp . "/" . $ver . "/");
-    array_shift($runs);
-    array_shift($runs);
-    // array_pop($runs);
-    // array_pop($runs);
-    // $jsonify_ver = array()
-    $runList = array();
-    foreach ($runs as &$run) {
-      if (substr($run, 0, 3) == "Run") {
-        $runList[] = $run;
-      } else {
-        continue;
-      }
-      // echo "       " . $run . "<br>";
-    }
-    $RunNumVer = array(
-      'Version' => $ver,
-      'Runs'=>$runList
-    );
-    $runpData[] = $RunNumVer;
-    // $runpData[] = json_encode($RunNumVer);
-  }
-  // echo "===================<br>";
-  $path_parse = array(
-    'RunPeriod' => $runp,
-    'Versions' => $runpData
-  );
-  // echo json_encode($path_parse);
-  // echo '<br>';
-  // $data[] = json_encode($path_parse);
-  $data[] = $path_parse;
-}
-
-echo json_encode($data, JSON_PRETTY_PRINT);
-// foreach ($data as $datum) {
-//   echo json_encode($datum);
-//   echo '<br>';
-// }
-
-$content = file_get_contents('../test2.json');
-
-file_put_contents("/u/group/halld/www/halldweb/html/data_monitoring/debug.txt", json_encode($data));
-file_put_contents("/u/group/halld/www/halldweb/html/data_monitoring/debug2.txt", $content);
-
-return $content;
 // return json_encode($data);
-// clean dirs
+return $data_json;
 
 ?>
--- mk_json.py
+++ mk_json.py
@@ -11,14 +11,15 @@ for x in glob('/work/halld2/data_monitoring/*'):
   for y in glob(x + '/*'):
     dict1 = {'Version':y.split('/')[-1]}
     runlist = []
-    for z in glob(y + '/*'):
+    for z in glob(y + '/Run*'):
       runlist.append(z.split('/')[-1])
     dict1['Runs'] = runlist
-  ver_list.append(dict1)
+    ver_list.append(dict1)
   dict0['Versions'] = ver_list
   list0.append(dict0)
 
-print list0
+str_list0 = str(list0).replace("'", '"')
+print str_list0
 
 fw = open('test.json', 'w')
 json.dump(list0, fw, indent = 4)
