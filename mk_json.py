c1ff9bb03b7fccca26635ab1b8dbab45ae2e6bb5
Keigo MIZUTANI <keigo@jlab.org>
2019-12-18 01:05:50 -0500

    Add mk_json.py

---
+++ mk_json.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+from glob import glob
+
+list0 = []
+for x in glob('/work/halld2/data_monitoring/*'):
+  dict0 = {'RunPeriod':x.split('/')[-1]}
+  ver_list = []
+  for y in glob(x + '/*'):
+    dict1 = {'Version':y.split('/')[-1]}
+    runlist = []
+    for z in glob(y + '/*'):
+      runlist.append(z.split('/')[-1])
+    dict1['Runs'] = runlist
+  ver_list.append(dict1)
+  dict0['Versions'] = ver_list
+  list0.append(dict0)
+
+print list0
