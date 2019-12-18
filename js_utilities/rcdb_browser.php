d67ea2636c96c50271a80383221333e1211f8c11
Keigo MIZUTANI <keigo@jlab.org>
2019-12-18 03:08:08 -0500

    Refactor rcdb_browser.php

--- js_utilities/rcdb_browser.php
+++ js_utilities/rcdb_browser.php
@@ -1,13 +1,7 @@
 <?php
 
-$data_json = file_get_contents('../test2.json');
-
-// $data = json_decode($data_json, true);
-
-// echo json_encode($data, JSON_PRETTY_PRINT);
-echo $data_json;
-
-// return json_encode($data);
-return $data_json;
+$data = file_get_contents('../test.json');
+echo $data;
+return $data;
 
 ?>
