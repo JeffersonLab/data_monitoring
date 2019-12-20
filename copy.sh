d346faa10fb8b5da0d6d7a88ed07ebad30a671b0
Keigo MIZUTANI <keigo@jlab.org>
2019-12-20 02:26:08 -0500

    Prepare js_utilities/BrowserFamily.py
    This is python2 script ..

--- copy.sh
+++ copy.sh
@@ -3,3 +3,4 @@
 cp Plot_Browser.html /group/halld/www/halldweb/html/data_monitoring/t.html
 cp js_utilities/rcdb_browser.php /group/halld/www/halldweb/html/data_monitoring/js_utilities/t.php
 cp mk_json.py /group/halld/www/halldweb/html/data_monitoring/
+cp js_utilities/BrowserFamily.py /group/halld/www/halldweb/html/data_monitoring/js_utilities/BrowserFamily.py
---
+++ js_utilities/BrowserFamily.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+import sys
+sys.path.append('/apps/bin/python')
+
+import MySQLdb
+import json
+
+# database access
+dbhost = 'hallddb.jlab.org'
+dbuser = 'monbrowser'
+dbname = 'BrowserFamily'
+dbcnx = MySQLdb.connect(host=dbhost, user=dbuser, db=dbname)
+dbcursor = dbcnx.cursor(MySQLdb.cursors.DictCursor)
+
+list0 = []
+dbcursor.execute("SELECT * FROM RunPeriods")
+runp_tup = dbcursor.fetchall()
+for x in sorted(runp_tup, key=lambda x: x['Name'], reverse=False):
+    dict0 = {'RunPeriod': x['Name']}
+    ver_list = []
+    dbcursor.execute("SELECT * FROM Versions where RunPeriod_ID=%d" % x['ID'])
+    ver_tup = dbcursor.fetchall()
+    for y in sorted(ver_tup, key=lambda x: x['Type'] + '%02d' % x['VersionNumber'], reverse=False):
+        dict1 = {'Version': y['Type'] + '_ver%02d' % y['VersionNumber']}
+        runlist = []
+        dbcursor.execute("SELECT * FROM Runs where Version_ID=%d" % y['ID'])
+        run_tup = dbcursor.fetchall()
+        for z in sorted(run_tup, key=lambda x: x['RunNumber'], reverse=False):
+            runlist.append('Run%06d' % z['RunNumber'])
+        dict1['Runs'] = runlist
+        if dict1['Runs']:
+            ver_list.append(dict1)
+    dict0['Versions'] = ver_list
+    list0.append(dict0)
+
+dbcnx.close()
+
+print json.dumps(list0)
