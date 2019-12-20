3b55063ca458f2b433a829aefc6af4134db07a1f
Keigo MIZUTANI <keigo@jlab.org>
2019-12-20 15:26:25 -0500

    Test commit
    Modify rcdb_brower.php
    Add DB connection sample snippet

--- js_utilities/rcdb_browser.php
+++ js_utilities/rcdb_browser.php
@@ -1,5 +1,24 @@
 <?php
 
+$servername = 'hallddb.jlab.org';
+$username   = 'monbrowser';
+$password   = '';
+$dbname     = 'BrowserFamily';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
+mysqli_select_db($conn, $dbname);
+$sql = 'SELECT * FROM RunPeriods';
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+file_put_contents('../debug.txt', $row['Name']);
+
+$conn->close();
+
 $data = shell_exec('python browser_family_to_json.py');
 echo $data;
 return $data;
