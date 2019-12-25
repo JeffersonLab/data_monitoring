efea924182bb95889c6ffcba808418b9fbcc5d57
Keigo MIZUTANI <keigo@jlab.org>
2019-12-25 12:41:34 -0500

    Make PopulateRunPeriodSelector2 work

--- js_utilities/populate_runperiod_selector2.php
+++ js_utilities/populate_runperiod_selector2.php
@@ -12,19 +12,20 @@ if (!$conn) {
     die('Connection failed: ' . mysqli_connect_error());
 }
 mysqli_select_db($conn, $dbname);
-$sql = 'SELECT * FROM RunPeriods';
+$sql = 'SELECT * FROM RunPeriods ORDER BY Name';
 $result = $conn->query($sql);
 $data = array();
 while ($row = $result->fetch_assoc()) {
-    $myarray[] = array(
+    $data[] = array(
         'ID' => $row['ID'],
         'Name' => $row['Name'],
         'Location_ID' => $row['Location_ID']
     );
 }
+// file_put_contents('../debug2.txt', json_encode($data));
 
 $conn->close();
 
-return json_encode($data));
+echo json_encode($data);
 
 ?>
