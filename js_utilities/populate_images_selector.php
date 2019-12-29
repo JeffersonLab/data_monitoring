d41775b005182d5ee14e2bbf2f6037eca5154c91
Keigo MIZUTANI <keigo@jlab.org>
2019-12-28 22:33:43 -0500

    Put an argument for populate_images_selector.php

--- js_utilities/populate_images_selector.php
+++ js_utilities/populate_images_selector.php
@@ -12,7 +12,7 @@ if (!$conn) {
     die('Connection failed: ' . mysqli_connect_error());
 }
 mysqli_select_db($conn, $dbname);
-$sql = 'SELECT * FROM PlotTypes';
+$sql = 'SELECT * FROM PlotTypes WHERE ID IN (SELECT PlotType_ID FROM Plots WHERE Run_ID IN (SELECT ID FROM Runs WHERE Version_ID=' . $_GET["ID"] . '))';
 $result = $conn->query($sql);
 $data = array();
 while ($row = $result->fetch_assoc()) {
