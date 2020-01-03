f265f446e6d09ac99884af13d8f4983357523132
Keigo MIZUTANI <keigo@jlab.org>
2020-01-03 11:43:02 -0500

    Add exception handling in PopulateImagesSelector

--- js_utilities/set_run_list_obj.php
+++ js_utilities/set_run_list_obj.php
@@ -12,7 +12,7 @@ if (!$conn) {
     die('Connection failed: ' . mysqli_connect_error());
 }
 mysqli_select_db($conn, $dbname);
-$sql = 'SELECT * FROM Runs WHERE Version_ID=' . $_GET["ID"] . ' ORDER BY RunNumber DESC';
+$sql = 'SELECT * FROM Runs WHERE Version_ID=' . $_GET["ID"] . ' ORDER BY RunNumber DESC LIMIT 20';
 $result = $conn->query($sql);
 $data = array();
 while ($row = $result->fetch_assoc()) {
