6d968d8a8b4c6d712c1fe9f7694dc7f34de1b515
Keigo MIZUTANI <keigo@jlab.org>
2020-01-07 21:52:26 -0500

    Add some spells

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp Plot_Browser.html /group/halld/www/halldweb/html/data_monitoring/
+cp Plot_Browser.html /group/halld/www/halldweb/html/data_monitoring/t.html
 cp js_utilities/populate_runperiod_selector.php /group/halld/www/halldweb/html/data_monitoring/js_utilities/populate_runperiod_selector.php
 cp js_utilities/populate_version_selector.php /group/halld/www/halldweb/html/data_monitoring/js_utilities/populate_version_selector.php
 cp js_utilities/populate_images_selector.php /group/halld/www/halldweb/html/data_monitoring/js_utilities/populate_images_selector.php
