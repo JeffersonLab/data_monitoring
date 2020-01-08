910a7668d2a39224f94ebd70284150ba2e44fa04
Keigo MIZUTANI <keigo@jlab.org>
2020-01-08 01:18:10 -0500

    Apply autopep8 for browser_rcdb_interface.py

--- _static/browser_rcdb_interface.py
+++ _static/browser_rcdb_interface.py
@@ -1,22 +1,21 @@
+import time
+import glob
+import rcdb
+import os.path
+import os
+import re
 import MySQLdb
 import sys
 import cgi
 import cgitb
 cgitb.enable()
 
-import re
 
-import os
-import os.path
 os.environ["RCDB_HOME"] = "/group/halld/www/halldweb/html/rcdb_home"
-import sys
 sys.path.append("/group/halld/www/halldweb/html/rcdb_home/python")
-import rcdb
 
 #from os import listdir
-import glob
 #from os.path import isfile, join
-import time
 
 db = rcdb.RCDBProvider("mysql://rcdb@hallddb/rcdb")
 
@@ -25,58 +24,58 @@ dbuser = 'datmon'
 dbpass = ''
 dbname = 'data_monitoring'
 
-conn=MySQLdb.connect(host=dbhost, user=dbuser, db=dbname)
-curs=conn.cursor(MySQLdb.cursors.DictCursor)
+conn = MySQLdb.connect(host=dbhost, user=dbuser, db=dbname)
+curs = conn.cursor(MySQLdb.cursors.DictCursor)
 
 
 def main(argv):
 
-    query=""
-    RunPeriod=""
-    RunMin=0
-    RunMax=99999
-    if len(argv)!=0:
-       query=argv[0]
-       RunPeriod=argv[1]
+    query = ""
+    RunPeriod = ""
+    RunMin = 0
+    RunMax = 99999
+    if len(argv) != 0:
+        query = argv[0]
+        RunPeriod = argv[1]
 
-
-    if(RunPeriod=="RunPeriod-2019-11"):
-        RunMin=70000
-        RunMax=79999
-    elif(RunPeriod=="RunPeriod-2019-01"):
-        RunMin=60000
-        RunMax=69999
-    elif(RunPeriod=="RunPeriod-2018-08"):
-        RunMin=50000
-        RunMax=59999
-    elif(RunPeriod=="RunPeriod-2018-01"):
-        RunMin=40000
-        RunMax=49999
-    elif(RunPeriod=="RunPeriod-2017-01"):
-        RunMin=30000
-        RunMax=39999
-    elif(RunPeriod=="RunPeriod-2016-10"):
-        RunMin=20000
-        RunMin=29999
-    elif(RunPeriod=="RunPeriod-2016-02"):
-        RunMin=10000
-        RunMax=19999
+    if(RunPeriod == "RunPeriod-2019-11"):
+        RunMin = 70000
+        RunMax = 79999
+    elif(RunPeriod == "RunPeriod-2019-01"):
+        RunMin = 60000
+        RunMax = 69999
+    elif(RunPeriod == "RunPeriod-2018-08"):
+        RunMin = 50000
+        RunMax = 59999
+    elif(RunPeriod == "RunPeriod-2018-01"):
+        RunMin = 40000
+        RunMax = 49999
+    elif(RunPeriod == "RunPeriod-2017-01"):
+        RunMin = 30000
+        RunMax = 39999
+    elif(RunPeriod == "RunPeriod-2016-10"):
+        RunMin = 20000
+        RunMin = 29999
+    elif(RunPeriod == "RunPeriod-2016-02"):
+        RunMin = 10000
+        RunMax = 19999
     else:
-        RunMin=0
-        RunMax=99999
+        RunMin = 0
+        RunMax = 99999
 
-    approvedRuns=[]
-    approvedRuns=db.select_runs(query,run_min=RunMin,run_max=RunMax)
-    approvedRunsRev=approvedRuns[::-1]    
-    approvedList=""
+    approvedRuns = []
+    approvedRuns = db.select_runs(query, run_min=RunMin, run_max=RunMax)
+    approvedRunsRev = approvedRuns[::-1]
+    approvedList = ""
 
     for ar in approvedRuns:
-        approvedList=approvedList+str(ar.number)+"_"
-    approvedList=approvedList[:-1]
+        approvedList = approvedList+str(ar.number)+"_"
+    approvedList = approvedList[:-1]
 
     print approvedList
 
     conn.close()
-                
+
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
