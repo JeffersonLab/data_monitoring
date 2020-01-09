85fdebb4742c603e5efd7cadc3745be88edbcdb3
Keigo MIZUTANI <keigo@jlab.org>
2020-01-08 20:29:29 -0500

    Update browser_rcdb_interface.py to use urllib.parse

--- _static/browser_rcdb_interface.py
+++ _static/browser_rcdb_interface.py
@@ -3,6 +3,7 @@
 
 import MySQLdb
 import sys
+import urllib.parse
 import cgitb
 cgitb.enable()
 import os
@@ -16,45 +17,32 @@ dbuser = 'datmon'
 dbname = 'data_monitoring'
 conn = MySQLdb.connect(host=dbhost, user=dbuser, db=dbname)
 
-query = ''
-RunPeriod = ''
-RunMin = 0
-RunMax = 99999
+qs = urllib.parse.parse_qs(sys.argv[1])
 
-for x in sys.argv[1].split('&'):
-    if x.startswith('query='):
-        query = x.split('=')[1].replace('%3E', '>').replace('%20', ' ')
-    elif x.startswith('RunP='):
-        RunPeriod = x.split('=')[1]
-    elif x.startswith('minRunNum='):
-        RunMin = int(x.split('=')[1])
-    elif x.startswith('maxRunNum='):
-        RunMax = int(x.split('=')[1])
+if 'minRunNum' not in qs or 'maxRunNum' not in qs:
+    if qs['RunP'][0] == "RunPeriod-2019-11":
+        qs['minRunNum'] = ['70000']
+        qs['maxRunNum'] = ['79999']
+    elif qs['RunP'][0] == "RunPeriod-2019-01":
+        qs['minRunNum'] = ['60000']
+        qs['maxRunNum'] = ['69999']
+    elif qs['RunP'][0] == "RunPeriod-2018-08":
+        qs['minRunNum'] = ['50000']
+        qs['maxRunNum'] = ['59999']
+    elif qs['RunP'][0] == "RunPeriod-2018-01":
+        qs['minRunNum'] = ['40000']
+        qs['maxRunNum'] = ['49999']
+    elif qs['RunP'][0] == "RunPeriod-2017-01":
+        qs['minRunNum'] = ['30000']
+        qs['maxRunNum'] = ['39999']
+    elif qs['RunP'][0] == "RunPeriod-2016-10":
+        qs['minRunNum'] = ['20000']
+        qs['maxRunNum'] = ['29999']
+    elif qs['RunP'][0] == "RunPeriod-2016-02":
+        qs['minRunNum'] = ['10000']
+        qs['maxRunNum'] = ['19999']
 
-if RunMin == 0 and RunMax == 99999:
-    if RunPeriod == "RunPeriod-2019-11":
-        RunMin = 70000
-        RunMax = 79999
-    elif RunPeriod == "RunPeriod-2019-01":
-        RunMin = 60000
-        RunMax = 69999
-    elif RunPeriod == "RunPeriod-2018-08":
-        RunMin = 50000
-        RunMax = 59999
-    elif RunPeriod == "RunPeriod-2018-01":
-        RunMin = 40000
-        RunMax = 49999
-    elif RunPeriod == "RunPeriod-2017-01":
-        RunMin = 30000
-        RunMax = 39999
-    elif RunPeriod == "RunPeriod-2016-10":
-        RunMin = 20000
-        RunMin = 29999
-    elif RunPeriod == "RunPeriod-2016-02":
-        RunMin = 10000
-        RunMax = 19999
-
-run_list = db.select_runs(query, run_min=RunMin, run_max=RunMax)
+run_list = db.select_runs(qs['query'][0], run_min=int(qs['minRunNum'][0]), run_max=int(qs['maxRunNum'][0]))
 if len(run_list) == 0:
     print('NaN')
 else:
