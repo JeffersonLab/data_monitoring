4c6cf118dbea4501fb8c8df37a849ce899c53026
Keigo MIZUTANI <keigo@jlab.org>
2020-03-09 15:36:57 -0400

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -4,3 +4,4 @@ cp Plot_Browser.html /group/halld/www/halldweb/html/data_monitoring/
 rm -rf /group/halld/www/halldweb/html/data_monitoring/_static
 cp -r _static /group/halld/www/halldweb/html/data_monitoring/
 cp htbin/data_monitoring/monitoring/runBrowser.py /group/halld/www/halldweb/htbin/data_monitoring/monitoring/
+cp ../my_sql_tables/add_filename.py /group/halld/www/halldweb/htbin/data_monitoring/monitoring/
