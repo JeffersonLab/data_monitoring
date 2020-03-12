8c089cb05d23f7c6c3f09e89fdc45b8e67dd76dc
Keigo MIZUTANI <keigo@jlab.org>
2020-03-12 17:26:32 -0400

    Check bitmasks in populate_images_selector.php

--- _static/populate_images_selector.php
+++ _static/populate_images_selector.php
@@ -12,8 +12,13 @@ if (!$conn) {
     die('Connection failed: ' . mysqli_connect_error());
 }
 mysqli_select_db($conn, $dbname);
-$sql = 'SELECT * FROM PlotTypes WHERE ID IN (SELECT PlotType_ID FROM Plots WHERE Run_ID IN (SELECT ID FROM Runs WHERE Version_ID=' . $_GET["ID"] . '))';
-# $sql = 'SELECT * FROM PlotTypes WHERE ID IN (SELECT PlotType_ID FROM Plots WHERE Run_ID IN (SELECT ID FROM Runs WHERE Version_ID=' . $_GET["ID"] . ')) AND (bitmask >> 3 AND "rawdata" IN (SELECT Type FROM Versions WHERE ID=' . $_GET["ID"] . '))';
+# $sql = 'SELECT * FROM PlotTypes WHERE ID IN (SELECT PlotType_ID FROM Plots WHERE Run_ID IN (SELECT ID FROM Runs WHERE Version_ID=' . $_GET["ID"] . '))';
+$bit4 = '(bitmask >> 3 = 0 AND "rawdata" IN (SELECT Type FROM Versions WHERE ID=' . $_GET["ID"] . '))';
+$bit2 = '(bitmask >> 1 & 1 = 0 AND "mon" IN (SELECT Type FROM Versions WHERE ID=' . $_GET["ID"] . '))';
+$bit1_mc = '(bitmask & 1 = 0 AND "mc" IN (SELECT Type FROM Versions WHERE ID=' . $_GET["ID"] . '))';
+$bit1_re = '(bitmask & 1 = 0 AND "recon" IN (SELECT Type FROM Versions WHERE ID=' . $_GET["ID"] . '))';
+$bit1_an = '(bitmask & 1 = 0 AND "analysis" IN (SELECT Type FROM Versions WHERE ID=' . $_GET["ID"] . '))';
+$sql = 'SELECT * FROM PlotTypes WHERE ID IN (SELECT PlotType_ID FROM Plots WHERE Run_ID IN (SELECT ID FROM Runs WHERE Version_ID=' . $_GET["ID"] . ')) AND NOT (' . $bit4 . ' OR ' . $bit2 . ' OR ' . $bit1_mc . ' OR ' . $bit1_re . ' OR ' . $bit1_an . ')';
 $result = $conn->query($sql);
 $data = array();
 while ($row = $result->fetch_assoc()) {
