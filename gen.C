2e5d67f41309b9b80e659fb9418bf547ad609c7f
Keigo MIZUTANI <keigo@jlab.org>
2020-03-17 23:45:50 -0400

    Remove gen.C (and is/)
    Add _static/no_plots.png

--- gen.C
+++
@@ -1,7 +0,0 @@
-void gen() {
-  TCanvas *c1 = new TCanvas();
-  TLatex *latex = new TLatex();
-  latex->SetTextSize(0.28);
-  latex->DrawLatexNDC(0.15, 0.37, "No Plots");
-  c1->Print("_static/no_plots.png");
-}
